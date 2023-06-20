06a636920ac8d5c58b50a981b9c613037ba06b7b
stuart howarth <stuarthowarth88@gmail.com>
2023-06-20 08:38:52 +0100

    started prototyping the flight and hotel price api's response

--- gptapp.py
+++ gptapp.py
@@ -1,34 +1,47 @@
 import os
 import openai
-
+import pdftotext
 
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
-text = 'svljbkadlvbalc'
+quote = 'New England Self Drive - A&K Dec 19.pdf'
 
 def generate_prompt(text):
-    return f"""For each of the hotels in this page from an itinerary can you tell me the check-in date of each hotel, check-out date of each hotel, hotel names, room category, number of rooms and board basis from the below text? If there is no hotel return no hotel
+    return f"""For each hotel on this page from an itinerary can you tell me the check-in date of each hotel, check-out date of each hotel, hotel names, room category, number of rooms and board basis from the below text?
 
-            {text}
+{text}
 
-            format:
-            Check-in Date:
-            Check-out Date:
-            Hotel Name:
-            Room Category:
-            Number of Rooms:
-            Board Basis:
+format:
+Check-in Date: day/month/year
+Check-out Date: day/month/year
+Hotel Name: if no name fill with N/A
+Room Category:
+Number of Rooms:
+Board Basis:
 
-            All hotels:
+All hotels:
 
-            No hotel"""
+"""
 
-response = openai.Completion.create(
-  model="text-davinci-003",
-  prompt=generate_prompt(text),
-  temperature=0,
-  max_tokens=2649,
-  top_p=1,
-  frequency_penalty=0,
-  presence_penalty=0
-  )
+
+all_responses = []
+
+with open(quote, "rb") as f:
+    pdf = pdftotext.PDF(f)
+
+# All pages
+for text in pdf:
+    response = openai.Completion.create(
+        model="text-davinci-003",
+        prompt=generate_prompt(text),
+        temperature=0,
+        max_tokens=2650,
+        top_p=1,
+        frequency_penalty=0,
+        presence_penalty=0
+        )
+    all_responses.append(response)
+    print(response)
+    print('................................')
+
+print(all_responses)
